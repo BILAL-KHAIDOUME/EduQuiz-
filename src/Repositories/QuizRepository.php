648c23a7302daaaedf7d2fd828145ecf919811c6
ayoubsofi <ayoub@email.com>
2026-05-13 12:58:35 +0100

    improve Quiz model by adding toArray method and simplify repository insert logic

--- src/Repositories/QuizRepository.php
+++ src/Repositories/QuizRepository.php
@@ -8,38 +8,15 @@ class QuizRepository
     {
     }
 
-    public function create(Quiz $quiz): bool
-    {
-        $sql = "
-            INSERT INTO quizzes
-            (
-                teacher_id,
-                title,
-                description,
-                access_code,
-                time_limit,
-                max_attempts
-            )
-            VALUES
-            (
-                :teacher_id,
-                :title,
-                :description,
-                :access_code,
-                :time_limit,
-                :max_attempts
-            )
-        ";
+   public function create(Quiz $quiz): bool
+{
+    $sql = "
+        INSERT INTO quizzes
+        (teacher_id, title, description, access_code, time_limit, max_attempts)
+        VALUES
+        (:teacher_id, :title, :description, :access_code, :time_limit, :max_attempts)
+    ";
 
-        $stmt = $this->pdo->prepare($sql);
-
-        return $stmt->execute([
-            ':teacher_id' => $quiz->getTeacherId(),
-            ':title' => $quiz->getTitle(),
-            ':description' => $quiz->getDescription(),
-            ':access_code' => $quiz->getAccessCode(),
-            ':time_limit' => $quiz->getTimeLimit(),
-            ':max_attempts' => $quiz->getMaxAttempts()
-        ]);
-    }
+    return $this->pdo->prepare($sql)->execute($quiz->toArray());
+}
 }
